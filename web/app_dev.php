c820f6e0c98c9f22a63e48d1a0d6d5eddce5bf07
Uwe Pries <uwe.pries@digartis.de>
2012-06-19 13:37:08 +0200

    - add fixtures and migrations dependecies

--- web/app_dev.php
+++ web/app_dev.php
@@ -2,12 +2,12 @@
 
 // if you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
-//umask(0000);
+umask(0000);
 
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    //|| isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
         '127.0.0.1',
         '::1',
