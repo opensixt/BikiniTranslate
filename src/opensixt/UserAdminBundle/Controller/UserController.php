c526f1070fa1240dfbc8518494f069c8094f4eed
Paul Seiffert <Paul.Seiffert@mayflower.de>
2012-08-07 18:16:08 +0200

    Implemented user creation

--- src/opensixt/UserAdminBundle/Controller/UserController.php
+++ src/opensixt/UserAdminBundle/Controller/UserController.php
@@ -8,10 +8,16 @@ use opensixt\UserAdminBundle\Form\UserEdit as UserEditForm;
 
 use opensixt\BikiniTranslateBundle\Entity\User;
 
-use \Symfony\Component\Security\Acl\Domain\ObjectIdentity;
-use \Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Acl\Domain\ObjectIdentity;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Acl\Permission\MaskBuilder;
+use Symfony\Component\Security\Acl\Domain\UserSecurityIdentity;
+use Symfony\Component\Security\Acl\Domain\RoleSecurityIdentity;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
+/**
+ * @author Paul Seiffert <paul.seiffert@mayflower.de>
+ */
 class UserController
 {
     /** @var \Symfony\Component\HttpFoundation\Request */
@@ -38,16 +44,16 @@ class UserController
     /** @var \Symfony\Bundle\FrameworkBundle\Routing\Router */
     public $router;
 
+    /** @var \Symfony\Component\Security\Acl\Model\MutableAclProviderInterface */
+    public $aclProvider;
+
     /**
      * @param int $page
-     * @throws AccessDeniedException
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function listAction($page = 1)
     {
-        if (!$this->securityContext->isGranted('ROLE_ADMIN')) {
-            throw new AccessDeniedException();
-        }
+        $this->requireAdminUser();
 
         $searchTerm = $this->request->get('search', '');
 
@@ -73,7 +79,7 @@ class UserController
     {
         $user = $this->getUserWithId($id);
 
-        if (!$this->securityContext->isGranted('VIEW', $user)) {
+        if (!($this->isAdminUser() || $this->securityContext->isGranted('VIEW', $user))) {
             throw new AccessDeniedException();
         }
 
@@ -93,7 +99,7 @@ class UserController
     {
         $user = $this->getUserWithId($id);
 
-        if (!$this->securityContext->isGranted('EDIT', $user)) {
+        if (!($this->isAdminUser() || $this->securityContext->isGranted('EDIT', $user))) {
             throw new AccessDeniedException();
         }
 
@@ -110,6 +116,72 @@ class UserController
                                                              array('user' => $user,
                                                                    'form' => $form->createView()));
         }
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function createAction()
+    {
+        $this->requireAdminUser();
+
+        $form = $this->getEditUserFormForUser();
+
+        return $this->templating->renderResponse('opensixtUserAdminBundle:User:create.html.twig',
+                                                 array('form' => $form->createView()));
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function saveNewAction()
+    {
+        $this->requireAdminUser();
+
+        $user = new User();
+
+        $form = $this->getEditUserFormForUser($user);
+        $form->bind($this->request);
+
+        if ($form->isValid()) {
+            if ($form->get('newPassword')->getData()) {
+                if ($form['confirmPassword']->getData() == $form['newPassword']->getData()) {
+                    $user->setPassword($form['newPassword']->getData());
+                }
+            }
+
+            $this->em->persist($user);
+            $this->em->flush();
+
+            $this->initAclForNewUser($user);
+
+            return $this->redirect($this->generateUrl('_admin_userlist'));
+        }
+
+        return $this->templating->renderResponse('opensixtUserAdminBundle:User:create.html.twig',
+                                                 array('form' => $form->createView()));
+    }
+
+    /**
+     * @param User $user
+     */
+    private function initAclForNewUser(User $user)
+    {
+        $acl = $this->aclProvider->createAcl(ObjectIdentity::fromDomainObject($user));
+
+        $userIdentity = new UserSecurityIdentity($user->getUsername(), get_class($user));
+        $mask = new MaskBuilder();
+        $mask->add('view')
+             ->add(256);
+
+        $acl->insertObjectAce($userIdentity, $mask->get());
+
+        $roleIdentity = new RoleSecurityIdentity('ROLE_ADMIN');
+        $mask->reset();
+        $mask->add('master');
+        $acl->insertObjectAce($roleIdentity, $mask->get());
+
+        $this->aclProvider->updateAcl($acl);
     }
 
     /**
@@ -138,7 +210,7 @@ class UserController
      * @param User $user
      * @return UserEditForm|\Symfony\Component\Form\FormInterface
      */
-    private function getEditUserFormForUser(User $user)
+    private function getEditUserFormForUser(User $user = null)
     {
         return $this->formFactory
                     ->create(new UserEditForm($this->translator), $user);
@@ -163,5 +235,23 @@ class UserController
     private function generateUrl($route, $parameters = array(), $absolute = false)
     {
         return $this->router->generate($route, $parameters, $absolute);
+    }
+
+    /**
+     * @throws AccessDeniedException
+     */
+    private function requireAdminUser()
+    {
+        if (!$this->isAdminUser()) {
+            throw new AccessDeniedException();
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    private function isAdminUser()
+    {
+        return $this->securityContext->isGranted('ROLE_ADMIN');
     }
 }
