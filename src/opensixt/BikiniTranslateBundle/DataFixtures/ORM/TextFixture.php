22838157a3d6b9144f3945ec8f6d56d3f9b20e70
Uwe Pries <uwe.pries@sixt.de>
2012-08-10 15:59:34 +0200

    use addTarget to add target

--- src/opensixt/BikiniTranslateBundle/DataFixtures/ORM/TextFixture.php
+++ src/opensixt/BikiniTranslateBundle/DataFixtures/ORM/TextFixture.php
@@ -29,13 +29,19 @@ class TextFixture extends AbstractFixture implements OrderedFixtureInterface {
         $text2->setResource($manager->merge($this->getReference('res-dummy')));
         $text2->setLocale($manager->merge($this->getReference('language-en_GB')));
         $text2->setUser($manager->merge($this->getReference('users-user')));
-        $text2->setTarget(array('Hello world2'));
+        $text2->addTarget('Hello world2');
 
         $manager->persist($text2);
         $manager->flush();
 
         // second text, rev 2
-        $text2->setTarget(array('Hello World2'));
+        $text2->addTarget('Hello World2');
+
+        $manager->persist($text2);
+        $manager->flush();
+
+        // second text, rev 3
+        $text2->addTarget('Hello World2!!!');
 
         $manager->persist($text2);
         $manager->flush();
