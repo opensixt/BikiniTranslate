9ce674ac50df043623aa648158e8a9345f64d14a
Dmitri Mansilia <dmitri.mansilia@sixt.com>
2012-09-06 12:51:22 +0200

    knp menu + freetexts

---
+++ src/opensixt/SxTranslateBundle/EventListener/ConfigureMenuListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace opensixt\SxTranslateBundle\EventListener;
+
+use opensixt\BikiniTranslateBundle\Menu\Event\Configure as ConfigureMenuEvent;
+
+class ConfigureMenuListener
+{
+    /** @var \Symfony\Component\Security\Core\SecurityContext */
+    public $securityContext;
+
+    /** @var \Symfony\Bundle\FrameworkBundle\Translation\Translator */
+    public $translator;
+
+    /**
+     * @param ConfigureMenuEvent $event
+     */
+    public function onMenuConfigure(ConfigureMenuEvent $event)
+    {
+        if ($this->securityContext->isGranted('ROLE_USER')) {
+            $menu = $event->getMenu();
+
+            $menu = $menu->addChild($this->translator->trans('menu.freetext'));
+
+            $menu->addChild($this->translator->trans('menu.addfreetext'), array('route' => '_sxfreetext_add'));
+            $menu->addChild($this->translator->trans('menu.editfreetext'), array('route' => '_sxfreetext_edit'));
+        }
+    }
+}
+
