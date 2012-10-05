333adb7cbbb59bcae080611e74c9517071177e7d
Uwe Pries <uwe.pries@sixt.de>
2012-10-05 17:58:38 +0200

    add fos-user-bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle,
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
             new WhiteOctober\BreadcrumbsBundle\WhiteOctoberBreadcrumbsBundle(),
             new Opensixt\BikiniTranslateBundle\OpensixtBikiniTranslateBundle(),
             new Opensixt\UserAdminBundle\OpensixtUserAdminBundle(),
