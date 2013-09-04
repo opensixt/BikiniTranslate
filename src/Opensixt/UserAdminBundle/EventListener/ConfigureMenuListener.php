7fd24d98521aa207d630a5c54a0369417230aa06
Dmitri Mansilia <dmitri.mansilia@sixt.com>
2013-09-04 11:10:28 +0000

    fix the visibility of admin menu

--- src/Opensixt/UserAdminBundle/EventListener/ConfigureMenuListener.php
+++ src/Opensixt/UserAdminBundle/EventListener/ConfigureMenuListener.php
@@ -17,18 +17,20 @@ class ConfigureMenuListener
      */
     public function onMenuConfigure(ConfigureMenuEvent $event)
     {
-        $menu = $event->getMenu();
+        if ($this->securityContext->isGranted('ROLE_USER')) {
+            $menu = $event->getMenu();
 
-        $menu = $menu->addChild($this->translator->trans('menu.administration'))
-            ->setAttribute('dropdown', true);
+            $menu = $menu->addChild($this->translator->trans('menu.administration'))
+                ->setAttribute('dropdown', true);
 
-        $menu->addChild($this->translator->trans('menu.users'), array('route' => '_admin_userlist'));
+            $menu->addChild($this->translator->trans('menu.users'), array('route' => '_admin_userlist'));
 
-        if ($this->securityContext->isGranted('ROLE_ADMIN')) {
+            if ($this->securityContext->isGranted('ROLE_ADMIN')) {
 
-            $menu->addChild($this->translator->trans('menu.groups'), array('route' => '_admin_grouplist'));
-            $menu->addChild($this->translator->trans('menu.languages'), array('route' => '_admin_langlist'));
-            $menu->addChild($this->translator->trans('menu.resources'), array('route' => '_admin_reslist'));
+                $menu->addChild($this->translator->trans('menu.groups'), array('route' => '_admin_grouplist'));
+                $menu->addChild($this->translator->trans('menu.languages'), array('route' => '_admin_langlist'));
+                $menu->addChild($this->translator->trans('menu.resources'), array('route' => '_admin_reslist'));
+            }
         }
     }
 }
